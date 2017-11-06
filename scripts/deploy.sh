d0d56a76071d57d87002919adf93505dbbac6109
Florent MONTHEL <fmonthel@flox-arts.net>
2017-11-06 10:52:11 -0500

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
  
 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no flaprdmon01@flaprdmon01.flox-arts.in <<EOF
-  cd /srv/flaprdmon01/tools/MyNagiosClientMng
+  cd tools/MyNagiosClientMng
   git reset --hard origin/master
   git pull origin
   exit
