95f54d7082a0105ea6709e573a0db224b411873f
Florent MONTHEL <fmonthel@flox-arts.net>
2017-11-06 21:33:28 -0500

    Delete deploy.sh

--- scripts/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
- 
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no flaprdmon01@flaprdmon01.flox-arts.in <<EOF
-  cd tools/MyNagiosClientMng
-  git reset --hard origin/master
-  git pull origin
-  exit
-EOF
